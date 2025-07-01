e7255f5be31aa2709f1190c27fbf9c0bfeb91572
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-07-01 21:11:52 +0500

    let, const, var

---
+++ 01.variables.js
@@ -0,0 +1,11 @@
+let playerName = "Cristiano Ronaldo"
+const shirtNumber = 21
+var playerPosition = "Striker"
+playerCountry = "Portugal"
+
+// shirtNumber = 7
+// TypeError: Assignment to constant variable
+
+console.log(shirtNumber);
+
+console.table([playerName, shirtNumber, playerPosition, playerCountry])
