8208ce88317066065a18979577ab29837f5956d7
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-07-14 23:56:30 +0500

    Stack in JS

---
+++ 07.memory_stack.js
@@ -0,0 +1,12 @@
+// Stack is used in Primitive types
+// it gives a copy
+
+let myName = "Mohsin"
+
+let anotherName = myName
+anotherName = "Khan"
+
+console.log(myName);
+console.log(anotherName);
+
+// myName didn't change to Khan because it gives a copy to anotherName
