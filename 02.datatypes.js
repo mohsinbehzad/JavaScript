5833abb881d33144c024cdc2fb1b643825d15193
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-07-22 23:52:25 +0500

    datatypes used in js added

---
+++ 02.datatypes.js
@@ -0,0 +1,26 @@
+"use strict"  // treat all JS code as newer version
+
+// alert(2+2) // it will run in browser(inspect, console) but not here
+
+// console.log("Mohsin") console.log(2+4)      // it gives error
+
+console.log("Mohsin"); console.log(2 + 4)
+
+// but better is
+
+console.log("Behzad")
+console.log(5 + 5)
+
+
+console.log(typeof "Mohsin")     // string
+console.log(typeof null);        // object
+console.log(typeof undefined);   // undefined
+
+let id = 43
+console.log(typeof id);          // number 
+
+const myName = "Khan"            // string
+console.log(typeof myName)
+
+var myID = "Checking"
+console.log(typeof myID);        // string
