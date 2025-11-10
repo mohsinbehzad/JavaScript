a92e20d6d1c2c5013135159f8cb7cd216e0c13d7
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-11-10 21:43:44 +0500

    while loop added

---
+++ 32.while_loop.js
@@ -0,0 +1,20 @@
+let val = 5
+while (val > 0) {
+    console.log(val);
+    val--
+}
+
+// array iteration by while loop
+const myArray = ["Ronaldo", "Messi", "Ramos", "Pepe"]
+arr = 0
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr++
+}
+
+// do while loop
+score = 9
+do {
+    console.log(`Score is ${score}`);
+
+} while (score < 0);
