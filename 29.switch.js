15716a073b155ce7294d45a0566f148cfdc71a2e
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-11-10 21:40:55 +0500

    switch added

---
+++ 29.switch.js
@@ -0,0 +1,43 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const fruit =2
+
+switch (fruit) {
+    case 1:
+        console.log("Mangoe");
+        break;
+    case 2:
+        console.log("Apple");
+        break;                              // if you don't use break then it will execute below code as well.
+    case 3 :
+        console.log("Banana");
+        break;
+
+    default:
+        console.log("no fruit");
+        break;
+}
+
+const player = "Ronaldo"
+
+switch (player) {
+    case "Messi":
+        console.log(10);
+        break;
+    case "Ronaldo":
+        console.log(7);
+        break;
+    case "Ramos":
+        console.log(4);
+        break;
+
+    default:
+        break;
+}
