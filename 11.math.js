e6369fc2a5b980d8805c9400b8a2e8e10db67fdf
mohsinbehzad <mohsinbehzad14@gmail.com>
2025-11-18 23:41:20 +0500

    Math functions added

---
+++ 11.math.js
@@ -0,0 +1,16 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.67));
+console.log(Math.ceil(4.67));
+console.log(Math.floor(4.67));
+
+console.log(Math.min(3, 7, 8, 2));
+console.log(Math.max(3, 7, 8, 2));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 5
+const max = 15
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
