074bdf0604b77fc2f65f16b2e66f3c25c56e728e
mohsinbehzad <mohsinbehzad14@gmail.com>
2025-12-01 23:00:48 +0500

    Object Literal added

---
+++ 47.object_literal.js
@@ -0,0 +1,17 @@
+const myObj = {
+    username: "Mohsin",
+    age: 23,
+    email: 'mohsinbehzad14@gmail.com',
+
+    getUserDetails: function() {
+        // console.log(username);                       // it will give error
+        // console.log(this.username);                  // Mohsin
+        console.log(this);
+        
+                
+    }
+}
+
+myObj.getUserDetails()
+
+console.log(this);                                        // {}
